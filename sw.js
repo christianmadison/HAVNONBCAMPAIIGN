ecd13302f1d47b274e6da091ca3f917d3f1284e2
CNOSIADAM <christianmadison@me.com>
2026-05-23 18:28:39 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// HAVN Service Worker
+const CACHE = 'havn-v1';
+const SHELL = ['/havn-onboarding/', '/havn-onboarding/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() =>
+      caches.match('/havn-onboarding/')
+    ))
+  );
+});
